15bec91850620671aac03a172a5efe92b768dbd6
Yusuf Usul <jj.starlord12@gmail.com>
2026-05-02 22:36:38 +0200

    base path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  // Verplaats het van experimental naar de root van het object
-  allowedDevOrigins: ["172.29.0.1", "localhost:3000"],
+const nextConfig = {
+  output: 'export',
+  // Alleen een basePath als we op GitHub Pages (productie) draaien
+  basePath: isProd ? '/ANB-TUINWERKEN' : '', 
+  // Dit zorgt dat ook de interne links (zoals afbeeldingen) meeschalen
+  assetPrefix: isProd ? '/ANB-TUINWERKEN/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
